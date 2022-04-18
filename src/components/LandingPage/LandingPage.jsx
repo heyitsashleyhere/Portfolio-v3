94743f5c592aca27b9b25289108fef53be307283
Ashley Jiang <ashleyjiang4@gmail.com>
2022-04-18 15:19:36 +0200

    landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,20 +2,32 @@ import { motion } from "framer-motion"
 import './LandingPage.scss';
 
 export default function LandingPage() {
-
     
     return (
         <section className="LandingPage">
-            <h1>Hey there <span className="wave-emoji">👋</span></h1>
-            <p>it's <span className="name">Ashley Jiang</span>,</p>
+            <h1>Hey there <span className="wave-emoji">👋</span>
+            </h1>
+            <p>it's <span className="name">Ashley Jiang</span></p>
             
             <div className="statement">
-                a 
-                    <div>web developer</div>
-                    {/* <div>mother</div>
-                    <div>cat lover</div>
-                    <div>problem solver</div>
-                    <div>sweet tooth</div> */}
+                <div>I am a</div>
+                <motion.div className="noun" >
+                    <motion.div className="webdev"
+                                animate={{ opacity: [0, 1, 0], y: [-50, 0, 0, 50] }}
+                                transition={{ repeat: Infinity, repeatDelay: 8, duration: 2, times: [0, 0.5, 0.7, 1] }}>web developer</motion.div>
+                    <motion.div className="mother"
+                                animate={{ opacity: [0, 1, 0], y: [-50, 0, 0, 50] }}
+                                transition={{ delay: 2, repeat: Infinity, repeatDelay: 8, duration: 2, times: [0, 0.5, 0.7, 1] }}>mother</motion.div>
+                    <motion.div className="catlover"
+                                animate={{ opacity: [0, 1, 0], y: [-50, 0, 0, 50] }}
+                                transition={{ delay: 4, repeat: Infinity, repeatDelay: 8, duration: 2, times: [0, 0.5, 0.7, 1] }}>cat lover</motion.div>
+                    <motion.div className="problemsolver"
+                                animate={{ opacity: [0, 1, 0], y: [-50, 0, 0, 50] }}
+                                transition={{ delay: 6, repeat: Infinity, repeatDelay: 8, duration: 2, times: [0, 0.5, 0.7, 1] }}>problem solver</motion.div>
+                    <motion.div className="sweettooth"
+                                animate={{ opacity: [0, 1, 0], y: [-50, 0, 0, 50] }}
+                                transition={{ delay: 8, repeat: Infinity, repeatDelay: 8, duration: 2, times: [0, 0.5, 0.7, 1] }}>sweet tooth</motion.div>
+                </motion.div>
             </div>
  
 
